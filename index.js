efcb38996b886c9a994731c145d5c7b84fd8c3f9
Naimul Islam <ms0413354@gmail.com>
2022-09-18 21:56:57 +0600

    Start rendom api

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.use(express.json());
 // create a route
 app.use('/', hellowRoute);
 
+// page Not found
+app.use('*', (req, res) => {
+	const { baseUrl } = req;
+	res.send(`<h1>${baseUrl} Route Not Found!!!</h1>`);
+});
+
 // app listen
 app.listen(Port, () => {
 	console.log(`Assinment 1 server is running: ${Port}`);
